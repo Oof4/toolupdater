8f3c10b2fc08a3a9f1e0322063cb4df67a4fdc1d
Oof4 <67525688+Oof4@users.noreply.github.com>
2020-09-19 15:32:37 +1000

    Create pyupdater.py

---
+++ pyupdater.py
@@ -0,0 +1,22 @@
+import update
+import updates
+import os
+import Local Disk (C:)
+
+----------
+if update
+use update
+--------
+
+-----------
+if updates
+use updates
+-----------
+
+any occuring 
+import to Local Disk (C:)
+then
+import to os
+
+
+finish
